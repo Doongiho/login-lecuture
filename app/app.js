4a613b9ae1a518be9f89400fb092bfcc8f757408
seojiho <happyluck0426@gamil.com>
2022-12-02 14:10:38 +0900

    폴더구조 최적화

--- app/app.js
+++ app/app.js
@@ -7,11 +7,11 @@ const path = require('path'); //path를 import한다
 
 
 //라우팅
-const home = require("./routes/home");
+const home = require("./src/routes/home");
 
 //웹세팅
 app.set('view engine', 'ejs'); //'ejs'탬플릿을 엔진으로 한다.
-app.set('views', path.join(__dirname, 'views')); //폴더, 폴더경로 지정
+app.set("views", "./src/views");
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
 
