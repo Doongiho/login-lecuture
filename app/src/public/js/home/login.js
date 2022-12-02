5079bfdfc72952ba6a1a48a8da97e25929af80bb
seojiho <happyluck0426@gamil.com>
2022-12-02 15:07:38 +0900

    돔을 이용하여 HTML 객체 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "use strict"
 
-console.log("hello");
-console.log('bye');
+const { login } = require("../../../routes/home/home.ctrl");
+
+const id = document.querySelector("#id"),
+pseord = document.querySelector("#psword"), 
+loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id: id.ariaValueMax,
+        psword:psword.value,
+    };
+    console.log(req);
+}
