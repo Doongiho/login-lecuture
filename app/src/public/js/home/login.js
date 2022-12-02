c9ed7dcf2ef3418d0005aaff3ac76e66c4776f78
seojiho <happyluck0426@gamil.com>
2022-12-02 15:51:55 +0900

    로그인을 요청하는 fatch 생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,17 +1,23 @@
 "use strict"
 
-const { login } = require("../../../routes/home/home.ctrl");
-
 const id = document.querySelector("#id"),
-pseord = document.querySelector("#psword"), 
-loginBtn = document.querySelector("button");
+    psword = document.querySelector("#psword"), 
+    loginBtn = document.querySelector("button");
 
 loginBtn.addEventListener("click", login);
 
-function login(){
+function login() {
     const req = {
-        id: id.ariaValueMax,
-        psword:psword.value,
+        id: id.value,
+        psword: psword.value,
     };
     console.log(req);
+    fetch("/login", {
+        method:"POST",
+        headers : {
+            "Content-Type":"application/json",
+        },
+        body: JSON.stringify(req),
+
+    });
 }
