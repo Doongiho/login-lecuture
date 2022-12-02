7689d6d8b5d00e4c208ee9fe52b5576f6ca9c99d
seojiho <happyluck0426@gamil.com>
2022-12-02 14:28:30 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -12,6 +12,7 @@ const home = require("./src/routes/home");
 //웹세팅
 app.set('view engine', 'ejs'); //'ejs'탬플릿을 엔진으로 한다.
 app.set("views", "./src/views");
+app.use(express.static(`${__dirname}/src/public`));
 
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드.
 
